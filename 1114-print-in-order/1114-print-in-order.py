2957b02e2cdcd4fc85550dee37691364ed00fb94
Lucky kumar <pattiesfav@gmail.com>
2026-07-25 21:12:55 +0530

    Time: 71 ms (22.33%), Space: 19.8 MB (95.88%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,22 @@
+from threading import Semaphore
+
+class Foo:
+    def __init__(self):
+        self.second_sem = Semaphore(0)
+        self.third_sem = Semaphore(0)
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.second_sem.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.second_sem.acquire()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.third_sem.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.third_sem.acquire()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
