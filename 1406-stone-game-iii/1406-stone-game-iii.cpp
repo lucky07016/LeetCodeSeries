a92dca2d6977cffcaaf45b095686e0c4e082d3fa
Lucky kumar <pattiesfav@gmail.com>
2026-08-03 19:40:28 +0530

    Time: 12 ms (86.94%), Space: 130.2 MB (97.03%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+
+        int n = stoneValue.size();
+        vector<int> dp(4, 0);
+        for (int i = n - 1; i >= 0; i--) {
+
+            int best = INT_MIN;
+            int sum = 0;
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                sum += stoneValue[i + k];
+                best = max(best, sum - dp[(i + k + 1) % 4]);
+            }
+
+            dp[i % 4] = best;
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+
+        if (dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
+    }
+};
