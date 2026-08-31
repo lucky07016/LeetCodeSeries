5274dbd72291680e8f6ee954d0187496d831046d
Lucky kumar <pattiesfav@gmail.com>
2026-08-31 23:30:02 +0530

    Time: 0 ms (100%), Space: 124.6 MB (60.57%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int first = -1;
+        int last = -1;
+        int minDist = INT_MAX;
+
+        int pos = 1;
+
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+
+        while (curr->next != nullptr) {
+            ListNode* next = curr->next;
+
+            bool critical =
+                (curr->val > prev->val && curr->val > next->val) ||
+                (curr->val < prev->val && curr->val < next->val);
+
+            if (critical) {
+                if (first == -1) {
+                    first = pos;
+                } else {
+                    minDist = min(minDist, pos - last);
+                }
+
+                last = pos;
+            }
+
+            prev = curr;
+            curr = next;
+            pos++;
+        }
+
+        if (first == last) {
+            return {-1, -1};
+        }
+
+        return {minDist, last - first};
+    }
+};
